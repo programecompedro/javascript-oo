ce441197af7abe28961ea2152bd80875d3707deb
= <=>
2021-10-06 23:06:05 -0300

    Criando minha primeira Classe Javascript

---
+++ index.js
@@ -0,0 +1,24 @@
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new Cliente();
+
+cliente1.nome = "Pedro";
+cliente1.cpf = 12345678904
+cliente1.agencia = 0001
+cliente1.saldo = 5.000
+
+console.log(cliente1)
+
+const cliente2 = new Cliente();
+
+cliente2.nome = "Alice";
+cliente2.cpf = 98765432145
+cliente2.agencia = 0002
+cliente2.saldo = 8.000
+
+console.log(cliente2)
