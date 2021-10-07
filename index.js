4393ee90f5d7e5815478bda9bf9d7981eb8d25cf
= <=>
2021-10-07 17:00:51 -0300

    comportamento de classes

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 class Cliente {
     nome;
     cpf;
+    
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor) {
+        if (this.saldo < valor){
+            return console.log("Valor é maior do que seu saldo")
+        } else {
+            return console.log(this.saldo - valor)
+        }
+    }
 }
 
 const cliente1 = new Cliente();
 
 cliente1.nome = "Pedro";
 cliente1.cpf = 12345678904
-cliente1.agencia = 0001
-cliente1.saldo = 5.000
-
-console.log(cliente1)
 
 const cliente2 = new Cliente();
 
 cliente2.nome = "Alice";
 cliente2.cpf = 98765432145
-cliente2.agencia = 0002
-cliente2.saldo = 8.000
 
+const contaCorrentePedro = new ContaCorrente()
+contaCorrentePedro.saldo = 0;
+contaCorrentePedro.agencia = 0001;
+contaCorrentePedro.saldo = 200
+console.log(contaCorrentePedro.saldo)
+contaCorrentePedro.sacar(100);
+
+console.log(cliente1)
 console.log(cliente2)
