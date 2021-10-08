037c84bed66f030610102ac53570cb44651c17a2
= <=>
2021-10-08 11:53:40 -0300

    melhorando usabilidade

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { Cliente } from "./Cliente.js"
-import { Conta } from "./Conta.js";
 import { ContaCorrente } from './ContaCorrente.js'
 import { ContaPoupanca } from './ContaPoupanca.js'
 
@@ -8,6 +7,10 @@ const cliente2 = new Cliente("Julia", "98765432145");
 
 const conta1 = new ContaCorrente(cliente1, 4000)
 const conta2 = new ContaPoupanca(1000, cliente2, 4000)
+
+conta1.depositar(100) // deposita 100$ na conta
+conta1.sacar(50) // saca 50$ da conta
+
 
 console.log(conta1) //Exibe dados da conta
 console.log(conta2) //Exibe dados da conta
