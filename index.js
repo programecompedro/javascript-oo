0e5d2f01c1e349916acbe673cf5515d6fc218ec9
= <=>
2021-10-08 12:07:48 -0300

    Chamando conta salário

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 import { Cliente } from "./Cliente.js"
 import { ContaCorrente } from './ContaCorrente.js'
 import { ContaPoupanca } from './ContaPoupanca.js'
+import { ContaSalario } from "./ContaSalario.js";
 
 const cliente1 = new Cliente("Julio", "48756778855");
-const cliente2 = new Cliente("Julia", "98765432145");
+//const cliente2 = new Cliente("Julia", "98765432145");
 
-const conta1 = new ContaCorrente(cliente1, 4000)
-const conta2 = new ContaPoupanca(1000, cliente2, 4000)
+//const conta1 = new ContaCorrente(cliente1, 4000)
+//const conta2 = new ContaPoupanca(1000, cliente2, 4000)
+const contasalario = new ContaSalario(cliente1)
+contasalario.sacar(10)
+//conta1.depositar(100) // deposita 100$ na conta
+//conta1.sacar(50) // saca 50$ da conta
 
-conta1.depositar(100) // deposita 100$ na conta
-conta1.sacar(50) // saca 50$ da conta
 
-
-console.log(conta1) //Exibe dados da conta
-console.log(conta2) //Exibe dados da conta
+console.log(contasalario) //Exibe dados da conta
+//console.log(conta2) //Exibe dados da conta
 
 
 
